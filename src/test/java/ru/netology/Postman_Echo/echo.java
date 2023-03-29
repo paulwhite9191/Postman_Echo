12611b28c004b0fd1eed4ce21265330b453e81f7
Paul White <paratrak@list.ru>
2023-03-29 11:29:04 +0300

    Body data broke

--- src/test/java/ru/netology/Postman_Echo/echo.java
+++ src/test/java/ru/netology/Postman_Echo/echo.java
@@ -19,7 +19,7 @@ class echo {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello, World!"))
+                .body("data", equalTo("Hello, Man!"))
 
         ;
     }
